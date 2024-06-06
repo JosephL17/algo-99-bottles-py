362482af58fc238ffa524d1dd314ec38ee997389
josephl17 <joseph.lauria17@gmail.com>
2024-06-06 18:04:10 -0500

    solution

--- bottles.py
+++ bottles.py
@@ -1,7 +1,18 @@
-def bottle_song():
-	# write your code here!
-	pass
+def bottle_song(num):
+	for i in range(num, 0, -1):
+		if i == 1:
+			bottle = 'bottle'
+			next_bottle = 'no more bottles'
+		else:
+			bottle = 'bottles'
+			next_bottle = 'bottle' if i - 1 == 1 else 'bottles'
 
-bottle_song()
+		print(f"{i} {bottle} of beer on the wall, {i} {bottle} of beer")
+		print(f"Take one down and pass it around, {i - 1 if i > 1 else 'no more'} {next_bottle} of beer on the wall.\n")
+
+		print("No more bottles of beer on the wall, no more bottles of beer.")
+		print("Go to the store and buy some more, 99 bottles of beer on the wall.")
+
+bottle_song(99)
 
 ####kldjsfalkfdajalskdfjdfls
